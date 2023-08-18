58495042cf3fc12093896b7b8304b32bd4823902
Nicolas Gasco <nicolas.gasco@qonto.com>
2023-08-18 16:31:31 +0200

    Improve name of script function

--- popup.js
+++ popup.js
@@ -1,14 +1,14 @@
-let clearNotificationsButtons = document.getElementById('clear-notifications');
+let clearNotificationsButtons = document.getElementById("clear-notifications");
 
-clearNotificationsButtons.addEventListener('click', async () => {
+clearNotificationsButtons.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setBorderColor,
+    function: clearLokaliseNotifications,
   });
 });
 
-function setBorderColor() {
-  document.querySelector('div.Toastify').remove();
+function clearLokaliseNotifications() {
+  document.querySelector("div.Toastify").remove();
 }
